bef2c9acb97a5d6c6eea4ac3e2ac916349cc7ec8
stayacid <stayacid@gmail.com>
2020-09-10 21:17:45 +0300

    fix merge

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/browser';
-import './index.css';
+import './scss/style.scss'; 
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 
