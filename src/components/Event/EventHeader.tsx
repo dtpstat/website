f1a840d2102975c22ab65432ceaaa061dadb4e5e
stayacid <stayacid@gmail.com>
2020-09-17 21:26:20 +0300

    making event header

--- src/components/Event/EventHeader.tsx
+++ src/components/Event/EventHeader.tsx
@@ -4,11 +4,25 @@ import SvgIcon from '../SvgIcon'
 const EventHeader = () => {
     return (
         <div className="event-header">
-            <button className="btn-gallery">
-                <span className="galley-count">
+            <button className="btn-gallery" style={{marginRight: '32px'}}>
+                <span className="gallery-count">
                     <SvgIcon name="photo"/>
+                    6 фото
                 </span>
             </button>
+            <div style={{width: '100%'}}>
+                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+                    <button className="btn-viewmap">
+                        <SvgIcon name="map"/>
+                        <span className="caption">Перейти на карту</span>
+                    </button>
+                    <label className="viewSwitch">
+                        <input type="checkbox" />
+                        <span className="viewSwitch-slider"></span>
+                    </label>
+                </div>
+
+            </div>
         </div>
     );
 };
