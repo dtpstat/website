b71600470e29551a9b1e99f8377d38959864cbe7
stayacid <stayacid@gmail.com>
2020-09-18 20:28:16 +0300

    end event header add color prop to svg component

--- src/components/SvgIcon.tsx
+++ src/components/SvgIcon.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-type Props = { name : string };
+type Props = { name: string; color?: string };
 
-const SvgIcon = ({ name } : Props) => {
+const SvgIcon = ({ name, color }: Props) => {
     return (
-      <svg className={`icon icon-${name}`}>
-          <use xlinkHref={`svg/sprite.svg#${name}`}></use>
-      </svg>
+        <svg className={`icon icon-${name}`} style={{ fill: color ? color : '' }}>
+            <use xlinkHref={`svg/sprite.svg#${name}`}></use>
+        </svg>
     );
 };
 
