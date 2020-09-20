8d38faa0237bb4d959eebe9fdc1ef9cdfae20c54
stayacid <stayacid@gmail.com>
2020-09-20 20:44:10 +0300

    add articles page

--- src/components/Event/EventTags.tsx
+++ src/components/Event/EventTags.tsx
@@ -11,41 +11,48 @@ const EventTags = () => {
                 marginBottom: '20px',
             }}
         >
-            <div className="category-tag" tabIndex={0} style={{ marginBottom: '6px' }}>
-                <button className="btn-rect">
-                    <span>Ясно</span>
-                </button>
-            </div>
-            <div className="category-tag" tabIndex={0} style={{ marginBottom: '6px' }}>
-                <button className="btn-rect">
-                    <span>Светлое время суток</span>
-                </button>
-            </div>
-            <div className="category-tag" tabIndex={0} style={{ marginBottom: '6px' }}>
-                <button className="btn-rect">
-                    <span>Покрытие — сухое</span>
-                </button>
-            </div>
-            <div className="category-tag" tabIndex={0} style={{ marginBottom: '6px' }}>
-                <button className="btn-rect">
-                    <span>Многоквартирные жилые дома</span>
-                </button>
-            </div>
-            <div className="category-tag" tabIndex={0} style={{ marginBottom: '6px' }}>
-                <button className="btn-rect">
-                    <span>Нерегулируемый пешеходный переход</span>
-                </button>
-            </div>
-            <div className="category-tag" tabIndex={0} style={{ marginBottom: '6px' }}>
-                <button className="btn-rect">
-                    <span>Выезд с прилегающей территории</span>
-                </button>
-            </div>
-            <div className="category-tag-middle" tabIndex={0} style={{ marginBottom: '6px' }}>
-                <button className="btn-rect">
-                    <span>Ещё 5 тегов...</span>
-                </button>
-            </div>
+            <button
+                className="btn-rect"
+                style={{ marginBottom: '6px', marginRight: '8px', borderRadius: '4px' }}
+            >
+                <span className="subtitle2">Ясно</span>
+            </button>
+            <button
+                className="btn-rect"
+                style={{ marginBottom: '6px', marginRight: '8px', borderRadius: '4px' }}
+            >
+                <span className="subtitle2">Светлое время суток</span>
+            </button>
+            <button
+                className="btn-rect"
+                style={{ marginBottom: '6px', marginRight: '8px', borderRadius: '4px' }}
+            >
+                <span className="subtitle2">Покрытие — сухое</span>
+            </button>
+            <button
+                className="btn-rect"
+                style={{ marginBottom: '6px', marginRight: '8px', borderRadius: '4px' }}
+            >
+                <span className="subtitle2">Многоквартирные жилые дома</span>
+            </button>
+            <button
+                className="btn-rect"
+                style={{ marginBottom: '6px', marginRight: '8px', borderRadius: '4px' }}
+            >
+                <span className="subtitle2">Нерегулируемый пешеходный переход</span>
+            </button>
+            <button
+                className="btn-rect"
+                style={{ marginBottom: '6px', marginRight: '8px', borderRadius: '4px' }}
+            >
+                <span className="subtitle2">Выезд с прилегающей территории</span>
+            </button>
+            <button
+                className="btn-rect"
+                style={{ marginBottom: '6px', marginRight: '8px', borderRadius: '4px' }}
+            >
+                <span className="subtitle2">Ещё 5 тегов...</span>
+            </button>
         </div>
     );
 };
--- src/components/News/NewsItem.tsx
+++ src/components/News/NewsItem.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import { Colors } from 'components/ui/Colors';
 
-type Props = { size?: string, title: string; img: string, link: string };
+type Props = {
+    title: string;
+    img: string;
+    link: string;
+    size?: string;
+    text?: string;
+    tags?: String[];
+};
 
-const NewsItem = ({ size, title, img, link }: Props) => {
+const NewsItem = ({ title, img, link, size, text, tags }: Props) => {
     return (
         <a className={size ? `news-item-${size}` : 'news-item'} href={link}>
             <span className="img-wrap">
-              <img src={img} alt={title}/>
+                <img src={img} alt={title} />
             </span>
+            {tags ? (
+                <div style={{ display: 'flex', flexWrap: 'wrap', marginBottom: '8px' }}>
+                    {tags.map((tag) => (
+                        <button
+                            className="btn-rect"
+                            style={{ borderRadius: '4px', marginRight: '8px' }}
+                        >
+                            <span className="subtitle3">{tag}</span>
+                        </button>
+                    ))}
+                </div>
+            ) : (
+                ''
+            )}
             <span className="h3">{title}</span>
+            {text ? <p className="body2">{text}</p> : ''}
         </a>
     );
 };
