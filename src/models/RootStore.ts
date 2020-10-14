b97edcbf304ee93af9576c81e2aff15578b82561
Alexey Ustinov <alvustinov@gmail.com>
2020-10-14 21:25:21 +0300

    Fixed URL for map postiion

--- src/models/RootStore.ts
+++ src/models/RootStore.ts
@@ -1,69 +1,63 @@
-import React from 'react';
-import { types, flow } from 'mobx-state-tree';
+import React from 'react'
+import { types, flow } from 'mobx-state-tree'
 // @ts-ignore
-import makeInspectable from 'mobx-devtools-mst';
-import { FilterStore } from './FilterStore';
-import { MapStore } from './MapStore';
-import { AreaStore } from './AreaStore';
-import { TrafficAccidentStore } from './TrafficAccidentStore';
-import { Bounds, Coordinate, Scale } from 'types';
+import makeInspectable from 'mobx-devtools-mst'
+
+import { Bounds, Coordinate, Scale } from 'types'
+
+import { FilterStore } from './FilterStore'
+import { MapStore } from './MapStore'
+import { AreaStore } from './AreaStore'
+import { TrafficAccidentStore } from './TrafficAccidentStore'
 
 const RootModel = types
-    .model('RootModel', {
-        filterStore: FilterStore,
-        mapStore: MapStore,
-        areaStore: AreaStore,
-        trafficAccidentStore: TrafficAccidentStore,
+  .model('RootModel', {
+    filterStore: FilterStore,
+    mapStore: MapStore,
+    areaStore: AreaStore,
+    trafficAccidentStore: TrafficAccidentStore,
+  })
+  .actions(function (self) {
+    const onBoundsChanged = flow(function* onBoundsChanged(
+      center: Coordinate,
+      scale: Scale,
+      bounds: Bounds
+    ) {
+      window.history.pushState(null, '', `?center=${center[0]}:${center[1]}&scale=${scale}`)
+      const { areaStore, filterStore, trafficAccidentStore } = self
+      yield areaStore.loadArea(center, scale)
+      const areaId = areaStore.area?.id
+      if (!areaId) {
+        // TODO clear filters?
+        return
+      }
+      yield filterStore.loadFiltersForArea(areaId)
+      trafficAccidentStore.loadTrafficAccidents(filterStore.startDate, filterStore.endDate, bounds)
+      yield areaStore.loadStatistics(center, scale, filterStore.startDate, filterStore.endDate)
     })
-    .actions(function (self) {
-        const onBoundsChanged = flow(function* onBoundsChanged(
-            center: Coordinate,
-            scale: Scale,
-            bounds: Bounds,
-        ) {
-            const { areaStore, filterStore, trafficAccidentStore } = self;
-            yield areaStore.loadArea(center, scale);
-            const areaId = areaStore.area?.id;
-            if (!areaId) {
-                // TODO clear filters?
-                return;
-            }
-            yield filterStore.loadFiltersForArea(areaId);
-            trafficAccidentStore.loadTrafficAccidents(
-                filterStore.startDate,
-                filterStore.endDate,
-                bounds,
-            );
-            yield areaStore.loadStatistics(
-                center,
-                scale,
-                filterStore.startDate,
-                filterStore.endDate,
-            );
-        });
 
-        const onTrafficAccidentsLoaded = (accidents: any) => {
-            self.mapStore.drawObjects(accidents);
-        };
+    const onTrafficAccidentsLoaded = (accidents: any) => {
+      self.mapStore.drawObjects(accidents)
+    }
 
-        return {
-            onBoundsChanged,
-            onTrafficAccidentsLoaded,
-        };
-    });
+    return {
+      onBoundsChanged,
+      onTrafficAccidentsLoaded,
+    }
+  })
 
 export const rootStore = RootModel.create({
-    filterStore: {},
-    mapStore: {},
-    areaStore: {},
-    trafficAccidentStore: {},
-});
+  filterStore: {},
+  mapStore: {},
+  areaStore: {},
+  trafficAccidentStore: {},
+})
 
-makeInspectable(rootStore);
+makeInspectable(rootStore)
 
-export const RootStoreContext = React.createContext<typeof rootStore>(rootStore);
+export const RootStoreContext = React.createContext<typeof rootStore>(rootStore)
 
 export function useStore() {
-    const store = React.useContext(RootStoreContext);
-    return store;
+  const store = React.useContext(RootStoreContext)
+  return store
 }
