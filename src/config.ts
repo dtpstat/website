b4cf111e36b1d36a473ba4cbec780cc2d72f9a38
Ivan Shumilin <ivanshumilin@gmail.com>
2021-09-14 03:05:44 +0300

    Relative paths

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export default {
-  API_URL: 'https://dtp-stat.ru/api',
-  STATIC_URL: 'https://dtp-stat.ru',
+  API_URL: `${process.env.REACT_APP_BASE_URL}/api`,
+  STATIC_URL: process.env.REACT_APP_BASE_URL,
   SENTRY_URL: 'https://2ab4410dbc6440a7ab3bfd0963c217d5@o412018.ingest.sentry.io/5288112',
 }
