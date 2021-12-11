f27d30b92d178d604eca3d405b805cf4b3e619f8
Alexander Kachkaev <alexander@kachkaev.ru>
2021-12-11 22:31:32 +0000

    Restrict default exports
    Ref: https://basarat.gitbook.io/typescript/main-1/defaultisbad

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,14 @@ module.exports = {
   extends: ["@kachkaev/eslint-config-react", "plugin:@next/next/recommended"],
   rules: {
     "@next/next/no-img-element": "off",
+    "import/no-default-export": "error",
   },
+  overrides: [
+    {
+      files: ["src/pages/**"],
+      rules: {
+        "import/no-default-export": "off",
+      },
+    },
+  ],
 };
