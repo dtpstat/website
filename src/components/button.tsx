e034562a5fbf2cfee6ad6dd2d2f5b4d23cb17f00
Sergei Smirnov <sergous@gmail.com>
2021-12-13 00:23:19 -0500

    Adds Button component

---
+++ src/components/button.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.15px;
+
+  margin: 0px 10px;
+  padding: 8px 16px;
+
+  background: #18334a;
+  border-radius: 20px;
+  border: none;
+  color: #ffffff;
+
+  &:active {
+    background: rgba(24, 51, 74, 0.72);
+  }
+`;
