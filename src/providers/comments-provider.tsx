8390beb652a422798710937c0b50b7e5f9443014
Sergei Smirnov <sergous@gmail.com>
2021-12-13 22:34:59 -0500

    Add comment provider

---
+++ src/providers/comments-provider.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { Comment } from "../types";
+
+interface CommentsContextValue {
+  newCommentText: string;
+  setNewCommentText: React.Dispatch<React.SetStateAction<string>>;
+  comments: Comment[];
+  setComments: React.Dispatch<React.SetStateAction<Comment[]>>;
+}
+
+const CommentsContext = React.createContext<CommentsContextValue | undefined>(
+  undefined,
+);
+
+export const CommentsProvider: React.FunctionComponent = ({ children }) => {
+  const [comments, setComments] = React.useState<Comment[]>([]);
+  const [newCommentText, setNewCommentText] = React.useState<string>("");
+
+  const providerValue = React.useMemo<CommentsContextValue>(
+    () => ({
+      newCommentText,
+      setNewCommentText,
+      comments,
+      setComments,
+    }),
+    [newCommentText, setNewCommentText, comments, setComments],
+  );
+
+  return (
+    <CommentsContext.Provider value={providerValue}>
+      {children}
+    </CommentsContext.Provider>
+  );
+};
+
+export const useComments = (): CommentsContextValue => {
+  const result = React.useContext(CommentsContext);
+  if (result === undefined) {
+    throw new Error("No CommentsContext value available");
+  }
+
+  return result;
+};
