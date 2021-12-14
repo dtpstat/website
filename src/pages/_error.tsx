33281fa6777c8c3e61f667e98e4f19a172ff3f24
Alexander Kachkaev <alexander@kachkaev.ru>
2021-12-14 10:56:01 +0000

    Add custom error page

---
+++ src/pages/_error.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+// @ts-expect-error -- temp code
+const Error = ({ statusCode, err }) => {
+  return (
+    <div>
+      <p>
+        {statusCode
+          ? `An error ${statusCode} occurred on server`
+          : "An error occurred on client"}
+      </p>
+      <code>{err}</code>
+    </div>
+  );
+};
+
+// @ts-expect-error -- temp code
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+
+  return { statusCode, err: `${err}` };
+};
+
+export default Error;
