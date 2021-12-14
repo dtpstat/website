6f67af17b42675dc13cd9d2928f6c67953f71c05
Alexander Kachkaev <alexander@kachkaev.ru>
2021-12-14 09:51:23 +0000

    Uncomment some code

--- src/pages/iframes/comments/[dtpId].tsx
+++ src/pages/iframes/comments/[dtpId].tsx
@@ -1,8 +1,8 @@
 import { GetServerSideProps, NextPage } from "next";
 // import Error from "next/error";
 import * as React from "react";
+import styled from "styled-components";
 
-// import styled from "styled-components";
 // import { CommentInput } from "../../../components/comment-input";
 // import { CommentList } from "../../../components/сomment-list";
 // import { commentsArePaused } from "../../../shared/helpersForComments";
@@ -13,23 +13,23 @@ export interface CommentsIframePageProps {
   comments?: Comment[];
 }
 
-// const CommentsHeader = styled.h2`
-//   font-family: Roboto;
-//   font-style: normal;
-//   font-weight: bold;
-//   font-size: 26px;
-//   line-height: 40px;
-//   color: #18334a;
-// `;
+const CommentsHeader = styled.h2`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 26px;
+  line-height: 40px;
+  color: #18334a;
+`;
 
 const CommentsIframePage: NextPage<CommentsIframePageProps> = ({
   dtpId,
   comments,
 }) => {
   return (
-    <div>
+    <CommentsHeader>
       hello {dtpId}, {comments?.length}
-    </div>
+    </CommentsHeader>
   );
   // if (!dtpId || !comments) {
   //   return <Error statusCode={404} />;
@@ -68,12 +68,10 @@ const CommentsIframePage: NextPage<CommentsIframePageProps> = ({
 
 export const getServerSideProps: GetServerSideProps<
   CommentsIframePageProps
-> = async () => {
-  // const rawDtpId = `${params?.dtpId}`;
-  // const parsedDtpId = parseInt(rawDtpId);
-  // const dtpId = `${parsedDtpId}` === rawDtpId ? parsedDtpId : 0;
-
-  const dtpId = 42;
+> = async ({ params }) => {
+  const rawDtpId = `${params?.dtpId}`;
+  const parsedDtpId = parseInt(rawDtpId);
+  const dtpId = `${parsedDtpId}` === rawDtpId ? parsedDtpId : 0;
 
   if (dtpId > 0) {
     // TODO: Check dtp id presence and return { notFound: true } on failure
