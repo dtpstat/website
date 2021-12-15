10487c909ab246f2d29b3013d06a8f22e2edd5c6
Sergei Smirnov <sergous@gmail.com>
2021-12-15 03:07:48 -0500

    Add login component

---
+++ src/components/link.tsx
@@ -0,0 +1,13 @@
+import NextLink from "next/link";
+import styled from "styled-components";
+
+export const Link = styled(NextLink)`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  letter-spacing: 0.15px;
+  text-decoration: underline;
+`;
