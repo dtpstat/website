0902ee059941c779ed755a2c55a70ba29f05c67d
Sergei Smirnov <sergous@gmail.com>
2021-12-15 03:45:03 -0500

    Add user data to comment input

--- src/components/comment-input.tsx
+++ src/components/comment-input.tsx
@@ -1,3 +1,4 @@
+import { useUser } from "@auth0/nextjs-auth0";
 import React, { ChangeEvent } from "react";
 import styled from "styled-components";
 
@@ -5,6 +6,7 @@ import { useComments } from "../providers/comments-provider";
 import { Comment } from "../types";
 import { AvatarImage } from "./avatar-image";
 import { Button } from "./button";
+import { Link } from "./link";
 import { TextInput } from "./text-input";
 
 const InputContainer = styled.div`
@@ -22,15 +24,20 @@ const InputContainer = styled.div`
 `;
 
 export const CommentInput: React.VoidFunctionComponent = () => {
+  const { user } = useUser();
   const { setNewCommentText, newCommentText, comments, setComments } =
     useComments();
 
+  const userPicture = (user && user.picture) || undefined;
+  const userName = (user && user.name) || "Anonymous";
+
   const handleSend = () => {
     const comment: Comment = {
-      id: comments.length,
-      user: "anon",
+      id: comments.length + 1,
+      user: userName,
       text: newCommentText,
       date: new Date().toUTCString(),
+      avatarUrl: userPicture,
     };
     setComments([...comments, comment]);
     setNewCommentText("");
@@ -39,9 +46,9 @@ export const CommentInput: React.VoidFunctionComponent = () => {
   const handleTextChange = (event: ChangeEvent<HTMLInputElement>) =>
     setNewCommentText(event.target.value);
 
-  return (
+  return user ? (
     <InputContainer>
-      <AvatarImage />
+      <AvatarImage src={userPicture} />
       <TextInput
         placeholder="Добавить комментарий..."
         value={newCommentText}
@@ -49,5 +56,10 @@ export const CommentInput: React.VoidFunctionComponent = () => {
       />
       <Button onClick={handleSend}>Отправить</Button>
     </InputContainer>
+  ) : (
+    <div>
+      Для оставления комментария{" "}
+      <Link href="/api/auth/login">авторизуйтесь</Link>.
+    </div>
   );
 };
