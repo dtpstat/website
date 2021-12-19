e2d358caed8ac859d1ab105d4b81ff618f27ed41
Sergei Smirnov <sergous@gmail.com>
2021-12-19 16:52:49 -0500

    Update comments api

--- src/pages/api/comments.tsx
+++ src/pages/api/comments.tsx
@@ -1,5 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { randomInt } from "crypto";
 import { NextApiHandler } from "next";
+
+import { Comment, User } from "../../types";
+
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient;
+}
 
 let prisma: PrismaClient;
 
@@ -15,17 +23,40 @@ if (process.env.NODE_ENV === "production") {
 const getComments = async () => {
   const comments = await prisma.comments.findMany({});
   const commentOne = comments[0];
-  const users = await prisma.users.findMany({
-    where: { id: commentOne.userId },
-  });
-  commentOne.user = users[0] ?? {};
+
+  commentOne.user = await getUser(commentOne.auth0userSub);
   commentOne.createDate = commentOne.createDate.toUTCString();
 
   return [commentOne];
 };
 
+const getUser = async (auth0userSub?: string): Promise<User> => {
+  const users = await prisma.users.findMany({
+    where: { auth0userSub },
+  });
+
+  return users[0] ?? {};
+};
+
+const addComment = async (newComment: Comment) => {
+  const user = await getUser(newComment.auth0userSub);
+  const comment: Comment = {
+    ...newComment,
+    id: randomInt(100000),
+    createDate: new Date().toUTCString(),
+    user,
+  };
+
+  return comment;
+};
+
 const handler: NextApiHandler = async (req, res) => {
-  res.status(200).json({ comments: await getComments() });
+  if (req.method === "POST") {
+    const comment = await addComment(JSON.parse(req.body) as Comment);
+    res.status(200).json({ comment });
+  } else {
+    res.status(200).json({ comments: await getComments() });
+  }
 };
 
 export default handler;
