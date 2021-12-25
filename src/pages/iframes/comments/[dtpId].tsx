de646fde397ba8fd66e997ba52bea207fd8184bc
Sergei Smirnov <sergous@gmail.com>
2021-12-25 18:54:41 -0500

    Move fetch comments to comments provider

--- src/pages/iframes/comments/[dtpId].tsx
+++ src/pages/iframes/comments/[dtpId].tsx
@@ -4,26 +4,20 @@ import * as React from "react";
 
 import { CommentInput } from "../../../components/comment-input";
 import { CommentList } from "../../../components/comment-list";
-import { fetchComments } from "../../../fetch/comments";
 import { CommentsProvider } from "../../../providers/comments-provider";
 import { commentsArePaused } from "../../../shared/helpersForComments";
-import { Comment } from "../../../types";
 
 export interface CommentsIframePageProps {
   dtpId?: number;
-  comments?: Comment[];
 }
 
-const CommentsIframePage: NextPage<CommentsIframePageProps> = ({
-  dtpId,
-  comments,
-}) => {
-  if (!dtpId || !comments) {
+const CommentsIframePage: NextPage<CommentsIframePageProps> = ({ dtpId }) => {
+  if (!dtpId) {
     return <Error statusCode={404} />;
   }
 
   return (
-    <CommentsProvider initComments={comments}>
+    <CommentsProvider>
       <CommentList />
 
       {commentsArePaused ? (
@@ -42,15 +36,11 @@ export const getServerSideProps: GetServerSideProps<
   const parsedDtpId = parseInt(rawDtpId);
   const dtpId = `${parsedDtpId}` === rawDtpId ? parsedDtpId : 0;
 
-  const baseUrl = global.process.env.AUTH0_BASE_URL as string;
-  const comments = await fetchComments(baseUrl);
-
   if (dtpId > 0) {
     // TODO: Check dtp id presence and return { notFound: true } on failure
     return {
       props: {
         dtpId,
-        comments,
       },
     };
   }
