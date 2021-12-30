74a62cb81a02cd5778994df66f60135ffc8f1e77
Sergei Smirnov <sergous@gmail.com>
2021-12-30 13:15:58 -0500

    Move prisma calls to services

---
+++ src/pages/api/users/[user-id].ts
@@ -0,0 +1,16 @@
+import { NextApiHandler } from "next";
+
+import { getUser, updateUser } from "../../../services/user";
+
+const handler: NextApiHandler = async (req, res) => {
+  const userId = req.query["userId"] as string;
+
+  if (req.method === "PATCH" && userId) {
+    const user = await updateUser(userId, JSON.parse(req.body));
+    res.status(200).json({ user });
+  } else if (req.method === "GET" && userId) {
+    res.status(200).json({ user: await getUser(userId) });
+  }
+};
+
+export default handler;
