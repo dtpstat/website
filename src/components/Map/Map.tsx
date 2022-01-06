baa174f071ba887f0b8ea7b3e5245a5c5079c891
clockblocker <asde3211q@gmail.com>
2022-01-06 14:49:49 +0300

    Heatmap integration

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,10 @@
-// import React, { useEffect, useCallback, useRef } from "react";
 import { observer } from "mobx-react";
-import React, { useRef } from "react";
+import Script from "next/script";
+import React, { useEffect, useRef, useState } from "react";
 import { Map as BaseMap, Placemark, YMaps } from "react-yandex-maps";
 import styled from "styled-components";
+
+import { data } from "./data";
 
 // import { useStore } from "../../models/RootStore";
 // import { debounce } from 'utils'
@@ -12,29 +14,80 @@ const StyledMap = styled(BaseMap)`
   width: 100%;
 `;
 
+const heatmapConfig = {
+  radius: 15,
+  dissipating: false,
+  opacity: 0.5,
+  intensityOfMidpoint: 0.5,
+  // gradient: {
+  //   0.1: 'rgba(128, 255, 0, 0.7)',
+  //   0.2: 'rgba(255, 255, 0, 0.8)',
+  //   0.7: 'rgba(234, 72, 58, 0.9)',
+  //   1.0: 'rgba(162, 36, 25, 1)',
+  // },
+  gradient: {
+    0.0: "rgba(126, 171, 85, 0.0)",
+    0.2: "rgba(126, 171, 85, 0.6)",
+    0.4: "rgba(255, 254, 85, 0.7)",
+    0.6: "rgba(245, 193, 66, 0.8)",
+    0.8: "rgba(223, 130, 68, 0.9)",
+    1.0: "rgba(176, 36, 24, 1)",
+  },
+};
+
 export const Map = observer(() => {
   const mapState = { center: [55.76, 37.64], zoom: 10 };
+  const [heatmapLoaded, setHeatmapLoaded] = useState(false);
   const mapRef = useRef(null);
-  // console.log(mapRef.current)
+
+  useEffect(() => {
+    if (heatmapLoaded) {
+      // ymaps.ready(['Heatmap']).then(function init() {
+      //   console.log('ymaps', ymaps)
+
+      //   const heatmap = new ymaps.Heatmap([], heatmapConfig);
+      //   console.log('heatmap', heatmap)
+      // });
+
+      window.ymaps.ready(["Heatmap"]).then(() => {
+        const hmdata = [];
+        for (let i = 0; i < data.length; i++) {
+          hmdata.push([
+            data[i].Cells.geoData.coordinates[1],
+            data[i].Cells.geoData.coordinates[0],
+          ]);
+        }
+
+        const heatmap = new window.ymaps.Heatmap(hmdata, heatmapConfig);
+
+        heatmap.setMap(mapRef.current);
+      });
+    }
+  }, [heatmapLoaded]);
 
   return (
-    <YMaps>
-      <StyledMap
-        defaultState={mapState}
-        instanceRef={mapRef}
+    <>
+      {/* <Script src="https://api-maps.yandex.ru/2.1/?apikey=ad7c40a7-7096-43c9-b6e2-5e1f6d06b9ec&lang=ru_RU" strategy="beforeInteractive" /> */}
+      <Script
+        id="ymaps-heatmap"
+        src="https://yastatic.net/s3/mapsapi-jslibs/heatmap/0.0.1/heatmap.min.js"
+        strategy="lazyOnload"
         onLoad={() => {
-          // console.log(mapRef.current)
+          setHeatmapLoaded(true);
         }}
-      >
-        <Placemark
-          geometry={[55.751574, 37.573856]}
-          properties={{
-            hintContent: "Собственный значок метки",
-            balloonContent: "Это красивая метка",
-          }}
-        />
-      </StyledMap>
-    </YMaps>
+      />
+      <YMaps>
+        <StyledMap defaultState={mapState} instanceRef={mapRef}>
+          <Placemark
+            geometry={[55.751574, 37.573856]}
+            properties={{
+              hintContent: "Собственный значок метки",
+              balloonContent: "Это красивая метка",
+            }}
+          />
+        </StyledMap>
+      </YMaps>
+    </>
   );
 });
 
