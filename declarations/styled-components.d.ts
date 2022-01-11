d4ebdca3984a86c479b5dc8d95b8f65fc7dad9f2
Alexander Kachkaev <alexander@kachkaev.ru>
2022-01-11 08:19:18 +0000

    Move `DefaultTheme` declaration to a separate folder, remove unused types

---
+++ declarations/styled-components.d.ts
@@ -0,0 +1,8 @@
+import "styled-components";
+
+// https://styled-components.com/docs/api#create-a-declarations-file
+declare module "styled-components" {
+  export interface DefaultTheme {
+    fontFamily: string;
+  }
+}
