169641a2bb9ef7b4023839a00b803398a925d68c
Alexander Kachkaev <alexander@kachkaev.ru>
2022-01-11 13:12:15 +0000

    Move `DefaultTheme` declaration to a separate folder, remove unused types (#17)

--- src/styles/default-theme.ts
+++ src/styles/default-theme.ts
@@ -1,19 +1,5 @@
 import { DefaultTheme } from "styled-components";
 
-declare module "styled-components" {
-  export interface DefaultTheme {
-    fontFamily: string;
-  }
-}
-
-export interface Theme {
-  fontFamily: string;
-}
-
-export interface ThemeProps {
-  theme: Theme;
-}
-
 export const defaultTheme: DefaultTheme = {
   fontFamily:
     "Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
