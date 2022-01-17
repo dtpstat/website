9578f433a6de27ff7aaa3f3350283de19e728699
Alexander Kachkaev <alexander@kachkaev.ru>
2022-01-17 01:05:15 +0000

    Try reducing git diff

--- src/components/inherited-map/inherited-map.tsx
+++ src/components/inherited-map/inherited-map.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable spaced-comment */
 import * as React from "react";
 
 import { FilterPanel } from "./components/filter-panel";
@@ -5,12 +6,12 @@ import { InfoPanel } from "./components/info-panel";
 import { Loader } from "./components/loader";
 import { Map } from "./components/map";
 import { rootStore, RootStoreContext } from "./models/root-store";
-// import Toast from 'components/Toast';
-// import Layers from './components/Layers/Layers';
-// import Event from './components/Event/Event';
-// import Modal from './components/Modal';
-// import Articles from './components/News/Articles';
-// import Article from './components/News/Article';
+//import Toast from 'components/Toast';
+//import Layers from './components/Layers/Layers';
+//import Event from './components/Event/Event';
+//import Modal from './components/Modal';
+//import Articles from './components/News/Articles';
+//import Article from './components/News/Article';
 
 export const InheritedMap: React.VoidFunctionComponent = () => (
   <RootStoreContext.Provider value={rootStore}>
