d5efb7598fb57cec4b7fd62b1242b8634d993c3a
Sergei Smirnov <sergous@gmail.com>
2022-01-21 19:26:31 -0500

    Separate e2e tests by extension

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,10 +1,6 @@
-import { chromium, expect, test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
-import {
-  loginWithEmail,
-  mockLoginWithEmail,
-  testAuthData,
-} from "./helpers/auth-helpers";
+import { mockLoginWithEmail, testAuthData } from "./helpers/auth-helpers";
 import { TestParams } from "./helpers/types";
 
 // eslint-disable-next-line jest/no-done-callback
@@ -19,43 +15,3 @@ test("mock login with email", async ({ page }: TestParams) => {
   // Confirm logged in user
   await expect(header).toHaveText(testAuthData.LOGIN);
 });
-
-// test("login with email", async () => {
-//   // Create a Chromium browser instance
-//   const browser = await chromium.launch();
-//   const context = await browser.newContext({
-//     locale: "ru-RU",
-//   });
-//   const page = await context.newPage();
-
-//   await loginWithEmail({ page });
-
-//   // Save storage state into the file.
-//   await context.storageState({ path: "tests/state.json" });
-
-//   // Go to /
-//   await page.goto(testAuthData.URL);
-
-//   const header = page.locator("h4");
-
-//   // Confirm logged in user
-//   await expect(header).toHaveText(testAuthData.LOGIN);
-// });
-
-// test("load auth0 session", async () => {
-//   // Create a Chromium browser instance
-//   const browser = await chromium.launch();
-//   const context = await browser.newContext({
-//     locale: "ru-RU",
-//     storageState: "tests/state.json",
-//   });
-//   const page = await context.newPage(); // Create a new context with the saved storage state.
-
-//   // Go to /
-//   await page.goto(testAuthData.URL);
-
-//   const header = page.locator("h4");
-
-//   // Confirm logged in user
-//   await expect(header).toHaveText(testAuthData.LOGIN);
-// });
