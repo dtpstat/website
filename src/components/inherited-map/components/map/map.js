c9e409524c74b50aa30542e7d1607e82b52a12fc
Alexander Kachkaev <alexander@kachkaev.ru>
2022-01-29 12:07:23 +0000

    Reset map.js

--- src/components/inherited-map/components/map/map.js
+++ src/components/inherited-map/components/map/map.js
@@ -1,20 +1,9 @@
 import { observer } from "mobx-react";
 import * as React from "react";
-import styled from "styled-components";
 
 import { useStore } from "../../models/root-store";
 import { StyledMap } from "./styles";
 // import { debounce } from 'utils'
-
-const ErrorMessage = styled.div`
-  position: absolute;
-  bottom: 20px;
-  font-size: 1.5rem;
-  left: 0;
-  right: 0;
-  text-align: center;
-  color: red;
-`;
 
 export const Map = observer(() => {
   const { mapStore } = useStore();
@@ -33,10 +22,6 @@ export const Map = observer(() => {
     [mapStore],
   );
   React.useEffect(() => {
-    if (!window.ymaps) {
-      return;
-    }
-
     window.ymaps.ready(["Heatmap"]).then(() => {
       const { center, zoom } = mapStore;
       const map = new window.ymaps.Map(
@@ -81,14 +66,6 @@ export const Map = observer(() => {
   }, [mapStore, boundsChangeHandler]);
 
   const mapRef = React.useRef();
-
-  if (!window.ymaps) {
-    return (
-      <ErrorMessage>
-        Яндекс.Карты не загрузились. Попробуйте обновить страницу.
-      </ErrorMessage>
-    );
-  }
 
   return <StyledMap id="map" ref={mapRef} />;
 });
