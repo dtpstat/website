b776f2171f6214f5f68c035a89d9c2df5d4cc1e4
Alexander Kachkaev <alexander@kachkaev.ru>
2022-01-30 00:22:26 +0000

    improve returnTo

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -4,13 +4,20 @@ import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = handleAuth({
   login: (req, res, { returnTo, ...rest } = {}) => {
+    // Enables redirects to a page where login was pressed
+    // Works together with
     const customReturnTo =
       typeof req.query["returnTo"] === "string"
         ? req.query["returnTo"]
         : undefined;
 
+    const customReturnToIsValid =
+      customReturnTo &&
+      customReturnTo.startsWith("/") && // e.g. /42
+      !customReturnTo.startsWith("//"); // e.g. //evil.example.com/42
+
     return handleLogin(req, res, {
-      returnTo: customReturnTo ?? returnTo,
+      returnTo: customReturnToIsValid ? customReturnTo : returnTo,
       ...rest,
     });
   },
