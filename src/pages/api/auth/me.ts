24a5dd607b66ef044cd135a5e757c87082bf17b1
Sergei Smirnov <sergous@gmail.com>
2022-01-31 05:24:43 -0500

    Update auth0 me path

--- src/pages/api/auth/me.ts
+++ src/pages/api/auth/me.ts
@@ -9,16 +9,20 @@ import { withSentry } from "@sentry/nextjs";
 import {
   createOrUpdateDbUser,
   userProfileToUser,
-} from "../../shared/user-helpers";
+} from "../../../shared/user-helpers";
 
 const handleProfile: HandleProfile = async (req, res) => {
   const { user: auth0User } = getSession(req, res) as { user: UserProfile };
 
   const userData = userProfileToUser(auth0User);
   const userId = auth0User.sub as string;
-  await createOrUpdateDbUser(userId, userData);
+  const updatedUser = await createOrUpdateDbUser(userId, userData);
 
-  return auth0HandleProfile(req, res);
+  if (req.method === "GET") {
+    res.status(200).json(updatedUser);
+  } else {
+    return auth0HandleProfile(req, res);
+  }
 };
 export { handleProfile };
 export default withSentry(handleProfile);
