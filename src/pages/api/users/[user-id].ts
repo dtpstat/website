f51d88f389d03788190191d7ffb2d2389c2bd3a2
Sergei Smirnov <sergous@gmail.com>
2022-01-31 03:21:11 -0500

    Update users api

--- src/pages/api/users/[user-id].ts
+++ src/pages/api/users/[user-id].ts
@@ -1,21 +1,46 @@
-import { withApiAuthRequired } from "@auth0/nextjs-auth0";
+import {
+  getSession,
+  UserProfile,
+  withApiAuthRequired,
+} from "@auth0/nextjs-auth0";
 import { User } from "@prisma/client";
 import { withSentry } from "@sentry/nextjs";
 import { NextApiHandler } from "next";
 
 import { getUser, updateUser } from "../../../services/user";
 
+interface Session {
+  user: UserProfile;
+}
+
 const handler: NextApiHandler = async (req, res) => {
   const userId = req.query["user-id"] as string;
+  const { user: sessionUser } = getSession(req, res) as Session;
 
-  if (req.method === "PATCH" && userId) {
+  if (!userId) {
+    res.status(422).json({ status: "query param user-id is missing" });
+
+    return;
+  }
+
+  if (userId !== sessionUser.sub) {
+    res.status(401).json({
+      status: `you are not authorized for access user-id '${userId}'`,
+    });
+
+    return;
+  }
+
+  if (req.method === "PATCH") {
     const user = await updateUser(
       userId,
       JSON.parse(req.body as string) as User,
     );
-    res.status(200).json({ user });
-  } else if (req.method === "GET" && userId) {
-    res.status(200).json({ user: await getUser(userId) });
+    res.status(200).json({ status: "ok", user });
+  } else if (req.method === "GET") {
+    res.status(200).json({ status: "ok", user: await getUser(userId) });
+  } else {
+    res.status(405).json({ status: "only PATCH and GET methods are allowed" });
   }
 };
 
