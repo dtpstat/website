f07072abf1d935f6b9be6b8edf24c40f2de23b08
Sergei Smirnov <sergous@gmail.com>
2022-02-01 06:35:26 -0500

    Set gravatar defaults

--- src/shared/user-helpers.ts
+++ src/shared/user-helpers.ts
@@ -1,7 +1,13 @@
 import { UserProfile } from "@auth0/nextjs-auth0";
-import gravatarUrl from "gravatar-url";
+import gravatarUrl, { Options } from "gravatar-url";
 
 import { User } from "../types";
+
+const defaultGravatarUrlOptions = {
+  size: 100,
+  rating: "g",
+  default: "mp",
+} as Options;
 
 export const convertUserProfileToUser = (userProfile: UserProfile): User => {
   return {
@@ -9,7 +15,9 @@ export const convertUserProfileToUser = (userProfile: UserProfile): User => {
     name: userProfile.name,
     nickname: userProfile.nickname,
     email: userProfile.email,
-    avatarUrl: userProfile.picture || gravatarUrl(userProfile.email!),
+    avatarUrl:
+      userProfile.picture ||
+      gravatarUrl(userProfile.email!, defaultGravatarUrlOptions),
     updateDate: userProfile.updated_at,
   } as User;
 };
