3495bad2c4916bbf0abcdb481d389d3d4ba512dd
Alexander Kachkaev <alexander@kachkaev.ru>
2022-02-06 13:26:28 +0000

    Cleanup

--- src/shared/prisma-helper.ts
+++ src/shared/prisma-helper.ts
@@ -3,13 +3,13 @@ import { PrismaClient } from "@prisma/client";
 // In development, the `next dev` clears Node.js cache on run. This in turn initializes
 // a new PrismaClient instance each time due to hot reloading that creates a connection
 // to the database. This can quickly exhaust the database connections as each PrismaClient
-// instance holds its own connection pool. Code below fixes that.
+// instance holds its own connection pool. Code below fixes this limitation.
 //
 // Source: https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
 // Permalink: https://github.com/prisma/docs/blob/0d887062857855c1225a0b953b608f6fea776e62/content/500-support/100-help-articles/400-nextjs-prisma-client-dev-practices.mdx
 
 declare global {
-  // eslint-disable-next-line no-var -- allow global `var` declarations
+  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
